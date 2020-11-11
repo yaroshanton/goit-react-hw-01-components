7963882013018f67fb0d7e725f7df2e5acbe448b
Anton Yarosh <on.yaroshanton@gmail.com>
2020-11-11 18:13:08 +0200

    almost done first task

---
+++ src/components/Profile.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Profile({ userData }) {
+    const { avatar, name, tag, location, stats } = userData;
+    return (
+        <div className="profile">
+            <div className="description">
+                <img
+                    src={avatar}
+                    alt="user avatar"
+                    className="avatar"
+                />
+                <p className="name">{name}</p>
+                <p className="tag">@{tag}</p>
+                <p className="location">{location}</p>
+            </div>
+
+            <ul className="stats">
+                <li>
+                    <span className="label">Followers: </span>
+                    <span className="quantity">{stats.followers}</span>
+                </li>
+                <li>
+                    <span className="label">Views: </span>
+                    <span className="quantity">{stats.views}</span>
+                </li>
+                <li>
+                    <span className="label">Likes: </span>
+                    <span className="quantity">{stats.likes}</span>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+Profile.propTypes = {
+    name: PropTypes.string.isRequired,
+};
+
+export default Profile;
