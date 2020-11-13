f57055abd36ba42d760a965e468790d9c68f02d1
Anton Yarosh <on.yaroshanton@gmail.com>
2020-11-13 17:55:35 +0200

    done profile. first task

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,33 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './Profile.module.css'
 
 function Profile({ user }) {
     const { avatar, name, tag, location, stats } = user;
     return (
-        <div className="profile">
-            <div className="description">
+        <div className={styles.profile}>
+            <div className={styles.description}>
                 <img
                     src={avatar}
                     alt="user avatar"
                     className="avatar"
                 />
-                <p className="name">{name}</p>
+                <p className={styles.name}>{name}</p>
                 <p className="tag">@{tag}</p>
                 <p className="location">{location}</p>
             </div>
 
-            <ul className="stats">
+            <ul className={styles.stats}>
                 <li>
-                    <span className="label">Followers: </span>
-                    <span className="quantity">{stats.followers}</span>
+                    <span className="label">Followers</span>
+                    <span className={styles.quantity}>{stats.followers}</span>
                 </li>
                 <li>
-                    <span className="label">Views: </span>
-                    <span className="quantity">{stats.views}</span>
+                    <span className="label">Views</span>
+                    <span className={styles.quantity}>{stats.views}</span>
                 </li>
                 <li>
-                    <span className="label">Likes: </span>
-                    <span className="quantity">{stats.likes}</span>
+                    <span className="label">Likes</span>
+                    <span className={styles.quantity}>{stats.likes}</span>
                 </li>
             </ul>
         </div>
